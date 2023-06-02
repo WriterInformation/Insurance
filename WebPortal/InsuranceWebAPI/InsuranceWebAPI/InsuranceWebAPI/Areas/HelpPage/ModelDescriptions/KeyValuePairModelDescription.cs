af7a148e2b3af5f8b64de19e331c7eeac06957fc
kp051662 <133110656+kp051662@users.noreply.github.com>
2023-06-02 18:48:23 +0530

    Delete WebPortal/InsuranceWebAPI/InsuranceWebAPI directory

--- WebPortal/InsuranceWebAPI/InsuranceWebAPI/InsuranceWebAPI/Areas/HelpPage/ModelDescriptions/KeyValuePairModelDescription.cs
+++
@@ -1,9 +0,0 @@
-namespace InsuranceWebAPI.Areas.HelpPage.ModelDescriptions
-{
-    public class KeyValuePairModelDescription : ModelDescription
-    {
-        public ModelDescription KeyModelDescription { get; set; }
-
-        public ModelDescription ValueModelDescription { get; set; }
-    }
-}
