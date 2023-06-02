5cefae6aef6d0d0000434b17a8c2391a8a7d1c0e
kp051662 <keyur.pandit@writerinformation.com>
2023-06-02 18:49:37 +0530

    Removed Web API application

--- WebPortal/InsuranceWebAPI/InsuranceWebAPI/InsuranceWebAPI/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace InsuranceWebAPI.Areas.HelpPage.ModelDescriptions
-{
-    public class DictionaryModelDescription : KeyValuePairModelDescription
-    {
-    }
-}
--- WebPortal/InsuranceWebAPI/InsuranceWebAPI/InsuranceWebAPI/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace InsuranceWebAPI.Areas.HelpPage.ModelDescriptions
-{
-    public class SimpleTypeModelDescription : ModelDescription
-    {
-    }
-}
